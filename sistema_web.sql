249ffec5fbdd4bc4d68c07368487540253aa9dd2
Calixto <142440440+Callixo@users.noreply.github.com>
2025-11-12 13:31:38 -0300

    Add files via upload

--- sistema_web.sql
+++ sistema_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3307
--- Tempo de geração: 01/10/2025 às 23:06
+-- Tempo de geração: 05/11/2025 às 23:09
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -39,7 +39,33 @@ CREATE TABLE `logs` (
 --
 
 INSERT INTO `logs` (`id`, `id_usuario`, `acao`, `data_hora`) VALUES
-(1, 1, 'Login realizado com sucesso.', '2025-10-01 17:12:11');
+(1, 1, 'Login 2FA bem-sucedido com a pergunta \'nascimento\'.', '2025-10-27 14:00:04'),
+(2, 1, 'Login 2FA bem-sucedido com a pergunta \'cep\'.', '2025-10-27 14:06:08'),
+(3, 1, 'Login 2FA bem-sucedido com a pergunta \'cep\'.', '2025-10-27 14:08:08'),
+(4, 1, 'Login 2FA bem-sucedido com a pergunta \'nascimento\'.', '2025-10-27 14:26:58'),
+(5, 1, 'Login 2FA bem-sucedido com a pergunta \'nascimento\'.', '2025-10-27 14:28:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `respostas_seguranca`
+--
+
+CREATE TABLE `respostas_seguranca` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `pergunta` varchar(255) NOT NULL,
+  `resposta` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `respostas_seguranca`
+--
+
+INSERT INTO `respostas_seguranca` (`id`, `id_usuario`, `pergunta`, `resposta`) VALUES
+(1, 1, 'pet', '$2y$10$Sxb1EWRII7JNC7MEO4IWO.popruXnuHF31yq520MXdJyNsRSi/2yO'),
+(2, 1, 'nascimento', '$2y$10$rqRgpXiIUel4lTj0Yl7HHOM9zFjEJRHofhxFOY0Aox05SIqcYneI2'),
+(3, 1, 'cep', '$2y$10$stGiuEOGVlyUs7WabH2CZOhh.mgzpXFF4yve4E62goa8jfhLJRDFu');
 
 -- --------------------------------------------------------
 
@@ -49,19 +75,27 @@ INSERT INTO `logs` (`id`, `id_usuario`, `acao`, `data_hora`) VALUES
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `nome_completo` varchar(80) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `nome_materno` varchar(80) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `telefone_celular` varchar(20) NOT NULL,
+  `telefone_fixo` varchar(20) NOT NULL,
+  `endereco_completo` varchar(255) NOT NULL,
+  `login` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `perfil` enum('comum','master') NOT NULL DEFAULT 'comum',
-  `data_cadastro` datetime DEFAULT current_timestamp()
+  `data_cadastro` datetime DEFAULT current_timestamp(),
+  `ativo` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `data_cadastro`) VALUES
-(1, 'renato santos calixto machado', 'calixtorenato935@gmail.com', '$2y$10$.QfXdic2YiiHTWljAc9Ca.zMHXOiRGphrDdlqK.Mel7Xs6MB1mHI.', 'comum', '2025-10-01 17:11:32');
+INSERT INTO `usuarios` (`id`, `nome_completo`, `data_nascimento`, `sexo`, `nome_materno`, `cpf`, `telefone_celular`, `telefone_fixo`, `endereco_completo`, `login`, `senha`, `perfil`, `data_cadastro`, `ativo`) VALUES
+(1, 'renato santos calixto machado', '2001-12-18', 'Masculino', 'nilzete machado', '192.147.877-27', '(+55)21-969464013', '(+55)21-69464013', 'rua valdir pequeno de melo 185', 'renatocalixto', '$2y$10$wvTXxOoRunoc3C2ZYsHpfeI79moynFUSMexaFD9/L099rnWTVSTSm', 'master', '2025-10-27 13:53:43', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -75,11 +109,19 @@ ALTER TABLE `logs`
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
+-- Índices de tabela `respostas_seguranca`
+--
+ALTER TABLE `respostas_seguranca`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
 -- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `cpf` (`cpf`),
+  ADD UNIQUE KEY `login` (`login`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -89,7 +131,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `respostas_seguranca`
+--
+ALTER TABLE `respostas_seguranca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
@@ -105,7 +153,13 @@ ALTER TABLE `usuarios`
 -- Restrições para tabelas `logs`
 --
 ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `respostas_seguranca`
+--
+ALTER TABLE `respostas_seguranca`
+  ADD CONSTRAINT `respostas_seguranca_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
